156fb046391b1227e2c0b822a8c0a9792ebae7b3
Lukasz Barcinski <lukasz.barcinski@enigma.com.pl>
2020-12-28 12:40:23 +0100

    Passing broadcast address to udp_node

--- rust/loc_share/src/main.rs
+++ rust/loc_share/src/main.rs
@@ -1,28 +1,37 @@
 use std::env;
-use std::str;
-use std::net::UdpSocket;
 mod udp_node;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
   let buf = &[1,2,3,4,5];
+  let my_ip = find_my_ip();
+  let broadcast_addr = find_broadcast_addr(my_ip);
 
-  let node = udp_node::UdpNode::new([192,168,0,105], [255,255,255,0]);
+  let node = udp_node::UdpNode::new(my_ip, broadcast_addr);
+  let port = find_free_port();
   if args.len() > 1 && args[1] == "send" {
     println!("sending broadcast");
-    node.broadcast_message(buf, 5555);
+    node.broadcast_message(buf, port);
   }
   else {
     println!("receiving broadcast");
-    receive_broadcast();
+    node.receive_broadcast(port);
   }
 }
 
-fn receive_broadcast() {
-  let socket = UdpSocket::bind("192.168.0.105:5555").expect("couldn't bind to address");
-  let mut buf = [0; 10];
-  let (bytes, src_addr) = socket.recv_from(&mut buf).expect("recv_from");
-  let word = str::from_utf8(&buf).unwrap();
-  println!("Received {} bytes: {:X?}", bytes, word);
-  println!("From {}", src_addr);
+fn find_my_ip() -> [u8; 4] {
+  //TODO:
+  return [127,0,0,1];
+  //[192,168,0,105]
+}
+
+fn find_free_port() -> u32 {
+  //TODO:
+  return 5555;
+}
+
+fn find_broadcast_addr(ip: [u8; 4]) -> [u8; 4] {
+  //TODO:
+  return [127,0,0,1];
+  //[255,255,255,0];
 }
--- rust/loc_share/src/udp_node.rs
+++ rust/loc_share/src/udp_node.rs
@@ -1,40 +1,44 @@
 use std::net::UdpSocket;
+use std::str;
 
 pub struct UdpNode {
   ip_addr: [u8; 4],
-  mask: [u8; 4],
+  broadcast_addr: [u8; 4],
 }
 
 impl UdpNode {
   
+  pub fn new(ip_addr: [u8; 4], broadcast_addr: [u8; 4]) -> UdpNode {
+    UdpNode {
+      ip_addr: ip_addr,
+      broadcast_addr: broadcast_addr,
+    }
+  }
+
   pub fn broadcast_message(&self, buf: &[u8], port: u32) {
     let broadcast_address = "0.0.0.0:0";
     let socket = UdpSocket::bind(broadcast_address).expect("couldn't bind to address");
     socket.set_broadcast(true).expect("set_broadcast");
-    let addr = self.make_broadcast_addr(port);
+    let addr = UdpNode::make_ip_addr(self.broadcast_addr, port);
     println!("Broadcasting {} bytes on address: {}", buf.len(), addr);
     socket.send_to(buf, addr).expect("couldn't send data");
   }
 
-  pub fn new(ip_addr: [u8; 4], mask: [u8; 4]) -> UdpNode {
-    UdpNode {
-      ip_addr: ip_addr,
-      mask: mask,
-    }
+  pub fn receive_broadcast(&self, port: u32) {
+    let addr = UdpNode::make_ip_addr(self.ip_addr, port);
+    let socket = UdpSocket::bind(addr).expect("couldn't bind to address");
+    let mut buf = [0; 10];
+    let (bytes, src_addr) = socket.recv_from(&mut buf).expect("recv_from");
+    let word = str::from_utf8(&buf).unwrap();
+    println!("Received {} bytes: {:X?}", bytes, word);
+    println!("From {}", src_addr);
   }
 
-  pub fn make_broadcast_addr(&self, port: u32) -> String {
-    let mut parts : Vec<String> = self.ip_addr.iter()
+  fn make_ip_addr(ip_addr: [u8;4], port: u32) -> String {
+    let parts : Vec<String> = ip_addr.iter()
       .map(|x| x.to_string())
       .collect();
-    for i in 0..parts.len() {
-      if self.mask[i] == 0 {
-        parts[i] = "255".to_string();
-      }
-      //TODO: values other than 255 and 0
-    }
-    let ip = parts.join(".");
-    return ip + ":" + &port.to_string();
+    return parts.join(".") + ":" + &port.to_string();
   }
 
 }
