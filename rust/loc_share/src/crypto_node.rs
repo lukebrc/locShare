52159e78533120ce770db618abef40446e6114f2
lbarcinski <lukasz.barcinski@enigma.com.pl>
2025-07-04 22:13:32 +0200

    Adding config args

--- rust/loc_share/src/crypto_node.rs
+++ rust/loc_share/src/crypto_node.rs
@@ -1,6 +1,7 @@
 extern crate hex;
 extern crate dirs;
-use std::{fs::File, io::{Read, Write}};
+use std::fs::File;
+use std::io::{Read, Write};
 use openssl::{hash::MessageDigest, pkcs5::pbkdf2_hmac};
 
 use openssl::aes::AesKey;
@@ -24,15 +25,15 @@ pub struct CryptoNode {
 
 impl CryptoNode {
 
-  pub fn has_config() -> bool {
-    match Self::load_config_from_disc() {
+  pub fn has_config(config_dir: &PathBuf) -> bool {
+    match Self::load_config_from_disc(config_dir) {
       Ok(_) => true,
       Err(_) => false
     }
   }
 
-  pub fn load_from_disc(pin: &str) -> Result<CryptoNode> {
-    let config = Self::load_config_from_disc()?;
+  pub fn load_from_disc(config_dir: &PathBuf, pin: &str) -> Result<CryptoNode> {
+    let config = Self::load_config_from_disc(config_dir)?;
     let dec_buf = Self::decrypt_sym_key(pin, &config.sym_key)?;
 
     Ok(CryptoNode {
@@ -42,7 +43,7 @@ impl CryptoNode {
     })
   }
 
-  pub fn create_new(pin: &str) -> Result<CryptoNode> {
+  pub fn create_new(config_dir: &PathBuf, pin: &str) -> Result<CryptoNode> {
     let mut sym_key = [0; CIPHER_LEN];
     openssl::rand::rand_bytes(&mut sym_key).unwrap();
     let enc_sym_key = Self::encrypt_sym_key(pin, &sym_key)?;
@@ -51,13 +52,12 @@ impl CryptoNode {
        invitation_code: Vec::new(),
        eph: Vec::new()
     };
-    Self::save_to_disc(&node)?;
+    Self::save_to_disc(config_dir, &node)?;
     Ok(node)
   }
 
-  fn load_config_from_disc() -> Result<Config> {
-    let loc_share_dir  = Self::create_config_dir_if_needed()?;
-    let config_path = loc_share_dir.as_path().join("config.pb");
+  fn load_config_from_disc(config_dir: &PathBuf) -> Result<Config> {
+    let config_path = config_dir.as_path().join("config.pb");
     let mut config_file = File::options().read(true).open(config_path)?;
     let mut buf: Vec<u8> = Vec::new();
     config_file.read_to_end(&mut buf)?;
@@ -65,8 +65,8 @@ impl CryptoNode {
       .or(Err(Self::io_error("Could not load config file")))
   }
 
-  fn save_to_disc(node: &CryptoNode) -> Result<()> {
-    let mut config_file = Self::create_or_open_config()?;
+  fn save_to_disc(config_dir: &PathBuf, node: &CryptoNode) -> Result<()> {
+    let mut config_file = Self::create_or_open_config(config_dir)?;
     let conf = Config {
       sym_key: node.sym_key.clone(),
       generated: 0,
@@ -80,9 +80,8 @@ impl CryptoNode {
     Ok(())
   }
 
-  fn create_or_open_config() -> Result<File> {
-    let loc_share_dir  = Self::create_config_dir_if_needed()?;
-    let config_path = loc_share_dir.as_path().join("config.pb");
+  fn create_or_open_config(config_dir: &PathBuf) -> Result<File> {
+    let config_path = config_dir.as_path().join("config.pb");
     if config_path.exists() {
       File::options().write(true).open(config_path)
         .or(Err(Self::io_error("Could not open config path")))
@@ -91,20 +90,6 @@ impl CryptoNode {
       File::create(config_path)
         .or(Err(Self::io_error("could not create config path")))
     }
-  }
-
-  fn create_config_dir_if_needed() -> Result<PathBuf> {
-    let loc_share_dir = match dirs::home_dir() {
-      Some(d) => d.as_path().join(".loc_share"),
-      None => return Err(Error::new(ErrorKind::NotFound, "Could not get home dir"))
-    };
-    let loc_share_dir_str = loc_share_dir.to_str().unwrap();
-    if ! std::fs::exists(&loc_share_dir)? {
-      println!("Creating directory {}", loc_share_dir_str);
-      std::fs::create_dir(&loc_share_dir)
-        .or(Err(Error::new(ErrorKind::Other, "Could not create loc_share directory")))?;
-    }
-    Ok(loc_share_dir)
   }
 
   fn io_error(msg: &str) -> Error {
@@ -177,7 +162,7 @@ impl CryptoNode {
 
   pub fn decrypt_ephmemeral_key(&self, eph_key: &Vec<u8>, inv_code: &Vec<u8>) -> Vec<u8> {
     //TODO: check conn_proc.broadcast_code CRC
-    println!("lengts: {}, {}", eph_key.len(), inv_code.len());
+    println!("lengths: {}, {}", eph_key.len(), inv_code.len());
     let decrypted = aes128_decrypt(&eph_key, &inv_code);
     return decrypted;
   }
--- rust/loc_share/src/main.rs
+++ rust/loc_share/src/main.rs
@@ -7,17 +7,27 @@ mod messages;
 mod proto;
 use crypto_node::CryptoNode;
 use std::env;
-use std::io::Result;
+use std::io::{Result, Error, ErrorKind};
+use std::path::PathBuf;
+use std::str::FromStr;
+use clap::Parser;
 
+#[derive(Parser, Debug)]
+struct Args {
+  #[arg(short, long)]
+  config_dir: String,
+}
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
+  let args = Args::parse();
   let buf = &[1,2,3,4,5];
   let my_ip = find_my_ip();
   let broadcast_addr = find_broadcast_addr(my_ip);
 
+  let loc_share_dir = get_loc_share_dir(args).unwrap();
+
   let unode = udp_node::UdpNode::new(my_ip, broadcast_addr);
-  let my_node = match create_crypto_node() {
+  let my_node = match create_crypto_node(&loc_share_dir) {
     Ok(n) => n,
     Err(msg) => panic!("{}", msg)
   };
@@ -35,21 +45,48 @@ fn main() {
   // }
 }
 
-fn create_crypto_node() -> Result<CryptoNode> {
-  if ! CryptoNode::has_config() {
+fn get_loc_share_dir(args: Args) -> Result<PathBuf> {
+  if args.config_dir.is_empty() {
+    return match dirs::home_dir() {
+      Some(d) => Ok(d.as_path().join(".loc_share")),
+      None => return Err(Error::new(ErrorKind::NotFound, "Could not get home dir"))
+    };
+  }
+  match PathBuf::from_str(args.config_dir.as_str()) {
+    Ok(d) => Ok(d),
+    Err(e) => Err(Error::new(ErrorKind::NotFound, format!("Invalid config directory: {}", e)))
+  }
+}
+
+fn create_crypto_node(config_dir: &PathBuf) -> Result<CryptoNode> {
+  if ! CryptoNode::has_config(config_dir) {
     println!("Create PIN to your node");
     let mut line = String::new();
     std::io::stdin().read_line(&mut line)?;
     let pin = line.trim_end();
-    CryptoNode::create_new(pin)
+    CryptoNode::create_new(config_dir, pin)
   }
   else {
     println!("Enter PIN to your node");
     let mut line = String::new();
     std::io::stdin().read_line(&mut line)?;
     let pin = line.trim_end();
-    CryptoNode::load_from_disc(pin)
+    CryptoNode::load_from_disc(config_dir, pin)
   }
+}
+
+fn create_config_dir_if_needed() -> Result<PathBuf> {
+  let loc_share_dir = match dirs::home_dir() {
+    Some(d) => d.as_path().join(".loc_share"),
+    None => return Err(Error::new(ErrorKind::NotFound, "Could not get home dir"))
+  };
+  let loc_share_dir_str = loc_share_dir.to_str().unwrap();
+  if ! std::fs::exists(&loc_share_dir)? {
+    println!("Creating directory {}", loc_share_dir_str);
+    std::fs::create_dir(&loc_share_dir)
+      .or(Err(Error::new(ErrorKind::Other, "Could not create loc_share directory")))?;
+  }
+  Ok(loc_share_dir)
 }
 
 fn find_my_ip() -> [u8; 4] {
